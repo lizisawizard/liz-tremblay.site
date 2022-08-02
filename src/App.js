20796af111b7def4f889214df10f82351483698e
Liz Tremblay <liz.tremblay@ibm.com>
2022-08-02 10:18:53 -0500

    (feat): Start on foundation - adding in React files, StickyNav component, styling, favicon

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './style/app.scss';
+
+import StickyNav from './components/StickyNav/StickyNav';
+
+const App = () => {
+    return (
+        <div className='App'>
+            <StickyNav></StickyNav>
+        </div>
+    );
+} 
+
+export default App;
---
+++ src/components/StickyNav/StickyNav.js
@@ -0,0 +1,17 @@
+import './stickyNav.scss';
+
+const StickyNav = () => {
+    <div className="stickyNav">
+        {/* fuppy uwu placeholder */}
+        <div className="stickyNav title">
+            <div className="stickyNav title--secondary">
+                <h3>Designer, developer, and champion of</h3>
+                <div className="vertical-line"/>
+                <h1>LIZ TREMBLAY</h1>
+                <div className="vertical-line"/>
+                <h3>light mode ✨</h3>
+            </div>
+        </div>
+    </div>
+}
+export default StickyNav;
