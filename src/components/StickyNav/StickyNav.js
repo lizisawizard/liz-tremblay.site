73e282bd5593239051e025220fb73503863e0140
Liz Tremblay <liz.tremblay@ibm.com>
2022-08-05 08:48:25 -0500

    StickyNav progress, content component, styling

--- src/components/StickyNav/StickyNav.js
+++ src/components/StickyNav/StickyNav.js
@@ -1,17 +1,39 @@
-import './stickyNav.scss';
+import '../../style/app.scss';
+import fuppy from '../../assets/fuppy-uwu.svg';
+import Instagram from '../../assets/Instagram.js';
+import Github from '../../assets/Github.js';
 
 const StickyNav = () => {
-    <div className="stickyNav">
-        {/* fuppy uwu placeholder */}
-        <div className="stickyNav title">
-            <div className="stickyNav title--secondary">
-                <h3>Designer, developer, and champion of</h3>
-                <div className="vertical-line"/>
-                <h1>LIZ TREMBLAY</h1>
-                <div className="vertical-line"/>
-                <h3>light mode ✨</h3>
+
+    // const 
+
+    return (
+        <div className='stickyNav'>
+            <div className='stickyNav--content'>
+                <img src={fuppy} alt='fuppy uwu'/>
+                <div className='stickyNav--title'>
+                    <div className='stickyNav--title-secondary'>
+                        <h3>Designer, developer, and champion of</h3>
+                        <div className='vertical-line'/>
+                        <span className='stickyNav--name'>
+                            <h1 className='stickyNav--name-L'>L</h1><h1 className='stickyNav--name-I'>I</h1><h1 className='stickyNav--name-Z'>Z</h1><h1>TREMBLAY</h1>
+                        </span>
+                        <div className='vertical-line'/>
+                        <h3 style={{ fontFamily: 'Open Sans Light', fontStyle: 'italic'}}>light mode ✨</h3>
+                    </div>
+                </div>
+                <div className='stickyNav--icons'>
+                    <a href='https://github.com/lizisawizard'><span className='icons'><Github/></span></a>
+                    <a href='https://www.instagram.com/lizisawizard/'><span className='icons'><Instagram/></span></a>
+                </div>
+                <div>
+                    <h2 className='stickyNav--tab edu'>EDU</h2>
+                    <h2 className='stickyNav--tab projects'>PROJECTS</h2>
+                    <h2 className='stickyNav--tab roles'>ROLES</h2>
+                </div>
+                <p style={{ fontSize: '24px' }}>Download my resume <a href='https://drive.google.com/file/d/1xVBYRMqFdERNyitzriM7u5FtuAJIZ7u9/preview' download>here</a>.</p>
             </div>
         </div>
-    </div>
+    );
 }
 export default StickyNav;
--- src/index.js
+++ src/index.js
@@ -5,12 +5,17 @@ import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <BrowserRouter>
+//     <Routes>
+//         <Route path="/" element={<App />} >
+//         </Route>
+//     </Routes>
+//   </BrowserRouter>
+// );
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Routes>
-        <Route path="/" element={<App />} >
-        </Route>
-    </Routes>
-  </BrowserRouter>
+  <App/>
 );
