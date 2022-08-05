a3bb845d5837382df1604b0aa08967303c7074fd
Liz Tremblay <liz.tremblay@ibm.com>
2022-08-05 08:48:25 -0500

    StickyNav progress, content component, styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './style/app.scss';
 
-import StickyNav from './components/StickyNav/StickyNav';
+import StickyNav from './components/StickyNav/StickyNav.js';
+import Content from './components/Content/Content.js';
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
     return (
         <div className='App'>
-            <StickyNav></StickyNav>
+            <StickyNav/>
+            <Content/>
+            <Outlet/>
         </div>
     );
 } 
---
+++ src/components/Content/Content.js
@@ -0,0 +1,12 @@
+import '../../style/app.scss';
+
+const Content = () => {
+    return (
+        <div className='content'>
+            <div className='content--edu'>
+                <h2 className='content--edu-title' style={{ fontSize: '36px' }}>EDU</h2>
+            </div>
+        </div>
+    );
+}
+export default Content;
