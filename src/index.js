cf1ae9788672c618197e168a85fda5fe92b0fbda
Liz Tremblay <liz.tremblay@ibm.com>
2022-10-16 23:59:08 -0400

    Revert "Reroute / to /home"
    This reverts commit 1a7b783f219ad32390297dc9cc2d900714a8c1b2.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style/app.scss';
 import App from './App';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Content from './components/Content/Content';
 import UserInterface from './components/UI/UserInterface';
 
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
-        <Route path="/" element={<App />}> 
+        <Route path="/" element={<App />} >
           <Route path="home" element={<Content/>}/>
           <Route path="user-interface" element={<UserInterface/>}/>
         </Route>
